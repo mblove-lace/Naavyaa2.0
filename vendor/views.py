f699b05f138c9fc294f63194183b928024b1e85f
M Bhattacherjee <mblovelace15@gmail.com>
2026-07-15 12:39:59 +0530

    Pre vendor commit

--- vendor/views.py
+++ vendor/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+# importing from .views import 
+# this import is used to import the views from the current directory's views.py file. What is current directory? The current directory is the directory where this file (vendor/views.py) is located.
+# It allows us to use the functions defined in views.py within this file.
+        # from django.contrib.auth.decorators import login_required
+        # from store import models as store_models
+        # from vendor import models as vendor_models
+        # from django.db import models
+
+        # @login_required
+        # def dashboard(request):
+        #     product = store_models.Product.objects.filter(vendor=request.user)
+        #     orders = store_models.OrderItem.objects.filter(vendors=request.user)
+        #     revenue = store_models.OrderItem.objects.filter(vendors=request.user).aggregate(total = models.Sum('total'))['total']
+        #     notis = vendor_models.Notification.objects.filter(vendor=request.user, seen=False)
+        #     reviews = store_models.Review.objects.filter(product__vendor=request.user)
+
+
